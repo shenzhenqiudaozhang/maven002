51e2189fc07e65ed44367dda09f94bc8f5969bc0
shenzhenqiudaozhang <shenzhenqiudaozhang@gmail.com>
2018-07-05 10:04:07 +0800

    我和git有个约会

---
+++ src/main/java/com/sz/Test1.java
@@ -0,0 +1,16 @@
+package com.sz;
+
+public class Test1 {
+
+
+//    public static void main(String[] args) {
+//        Test1 t  = new Test1();
+//        t.m1();
+//    }
+
+    // 加上junit的注解就可以了
+
+    public void m1() {
+        System.out.println("哈哈哈我是maven");
+    }
+}
---
+++ src/test/java/com/sz/Test1.java
@@ -0,0 +1,12 @@
+package com.sz;
+
+import org.junit.Test;
+
+public class Test1 {
+
+
+    @Test
+    public void test(){
+        System.out.println("junit test....");
+    }
+}
